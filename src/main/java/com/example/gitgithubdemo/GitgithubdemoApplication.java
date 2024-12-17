434f84862a3191e64c21bd857dec242210fb2237
thara-1234 <tharakrishna1989@gmail.com>
2024-12-17 10:49:21 +0530

    My first commit

---
+++ src/main/java/com/example/gitgithubdemo/GitgithubdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitgithubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgithubdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgithubdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitgithubdemo/GitgithubdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitgithubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitgithubdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
